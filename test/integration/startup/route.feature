841b4f097b5e7d0a355a0c970e15fb905324125e
coreywkruger <coreywkruger@gmail.com>
2016-05-12 08:25:22 -0700

    re-built tests; fixed bugs

--- test/integration/startup/route.feature
+++ test/integration/startup/route.feature
@@ -2,15 +2,17 @@ Feature: You should be able to call routes
 
   Scenario: A public route on the API is called without credentials
     Given a public route
-    When I call it without credentials
+    When I call route without credentials
     Then I should receive a response body
+    And I should get status code 200
 
   Scenario: A private route on the API is called with credentials
     Given a private route
-    When I call the private route it with credentials
+    When I call route with credentials
     Then I should receive a response body
+    And I should get status code 200
 
   Scenario: A private route on the API is called without credentials
     Given a private route
-    When I call the private route without credentials
-    Then I should receive an error from the API
+    When I call route without credentials
+    Then I should not get status code 200
--- test/integration/steps/route.js
+++ test/integration/steps/route.js
@@ -3,53 +3,45 @@ const expect = require('chai').expect;
 
 module.exports = function () {
   this.Given(/^a public route$/, function(cb){
+    this.req = request.get('/public/ping');
     cb();
   });
 
   this.Given(/^a private route$/, function(cb){
+    this.req = request.get('/private/ping');
     cb();
   });
 
-  this.When(/^I call it without credentials$/, function(cb){
-    request
-      .get('/public/ping')
-      .end((err, res) => {
-        this.err = err;
-        this.res = res;
-        cb();
-      });
+  this.When(/^I call route without credentials$/, function(cb){
+    this.req.end((err, response) => {
+      expect(err).to.be.null;
+      this.response = response;
+      cb();
+    });
   });
 
-
-  this.When(/^I call the private route it with credentials$/, function(cb){
-    request
-      .get('/public/ping')
-      .set('ping', 'pong')
-      .end((err, res) => {
-        this.err = err;
-        this.res = res;
-        cb();
-      });
-  });
-
-
-  this.When(/^I call the private route without credentials$/, function(cb){
-    request
-      .get('/private/ping')
-      .end((err, res) => {
-        this.err = err;
-        this.res = res;
-        cb();
-      });
+  this.When(/^I call route with credentials$/, function(cb){
+    this.req
+    .set('ping', 'pong')
+    .end((err, response) => {
+      expect(err).to.be.null;
+      this.response = response;
+      cb();
+    });
   });
 
   this.Then(/^I should receive a response body$/, function(cb){
-    expect(this.err).to.be.null;
+    expect(this.response.body).to.not.be.null;
     cb();
   });
 
-  this.Then(/^I should receive an error from the API$/, function(cb){
-    expect(this.res.statusCode).to.not.equal(200);
+  this.Then(/^I should not get status code 200$/, function(cb){
+    expect(this.response.statusCode).to.not.equal(200);
+    cb();
+  });
+
+  this.Then(/^I should get status code 200$/, function(cb){
+    expect(this.response.statusCode).to.equal(200);
     cb();
   });
 };
